270c3cd4ba4e442db669bdfc730275add839d4d0
Carson Davies <carsond11@gmail.com>
2021-01-20 22:08:25 -0700

    auth controller worked on

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -1,7 +1,27 @@
 const bcrypt = require('bcryptjs')
 
 module.exports = {
-  register: async (req, res) => { },
+  register: async (req, res) => {
+    const db = req.app.get('db')
+
+    const { username, email, password } = req.body
+
+    const [user] = await db.check_user([username])
+
+    if (user) {
+      return res.status(409).send('User already exists')
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+
+    const hash = bcrypt.hashSync(password, salt)
+
+    const [newUser] = await db.register_user([username, email, hash])
+
+    req.session.user = newUser
+
+    res.status(200).send(req.session.user)
+  },
 
   login: async (req, res) => { },
 
