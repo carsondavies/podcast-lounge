ddec63529d2b5264b094ff49b0985896fe71d555
Carson Davies <carsond11@gmail.com>
2021-02-23 13:21:41 -0700

    postman issue solved, endpoints fixed and tested

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -60,9 +60,6 @@ module.exports = {
       res.status(404).send('No session found')
     }
   },
-  random: (req, res) => {
-    res.status(200).send('endpoint check compelete')
-  }
 
 
 }                                                          
