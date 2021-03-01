e369992b66ddfcfc1567f8a8cb34dbfa0eb543dd
Carson Davies <carsond11@gmail.com>
2021-03-01 13:02:09 -0700

    header component a little built out, might need changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
 
+  const [variable, setVariable] = useState('')
+
+  const handleChange = (e) => {
+    setVariable(e.target.value)
+  }
+
   return (
-    <div>Welcome to Podcast Lounge</div>
+    <div>
+      <input
+        type='text'
+        name='variable'
+        value={variable}
+        onChange={handleChange}
+        placeholder='thank you here' />
+      <button>Enter here</button>
+    </div>
   )
 }
 
