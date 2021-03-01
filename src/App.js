6c9d65001f309fdda2358ca34de45d56c40b397c
Carson Davies <carsond11@gmail.com>
2021-02-28 23:13:46 -0700

    built out thank you component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Lounge from './Components/Lounge/Lounge';
 import routes from './routes'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Lounge />
       {routes}
     </div>
--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ThankYou = () => {
+  const [variable, setVariable] = useState('')
+
+  const handleChange = (e) => {
+    setVariable(e.target.value)
+  }
+
 
   return (
-    <div>ThankYou.js</div>
+    <div>
+      <input
+        type='text'
+        name='variable'
+        value={variable}
+        onChange={handleChange}
+        placeholder='thank you here' />
+      <button>Enter here</button>
+    </div>
   )
 }
 
